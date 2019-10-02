9e307385c9d61226fc25641dd5ee65a7973365ad
Shoaib Ahmad <ahmad.shoaib9121@gmail.com>
2019-10-02 19:02:03 +0500

    quantity buttons added and handled but offers still not quite up to the mark

--- src/app/pizza-order/pizza-order.component.ts
+++ src/app/pizza-order/pizza-order.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-pizza-order',
   templateUrl: './pizza-order.component.html',
   styleUrls: ['./pizza-order.component.scss']
 })
-export class PizzaOrderComponent implements OnInit {
+export class PizzaOrderComponent implements OnInit, AfterViewInit {
   constructor() { }
   offer1;
   offer2;
   offer3;
   lgdiscount;
   mddiscount;
-  showStriked;
+  smdiscount;
+  showDiscounted;
   toppings: any = {
     veg: [
       {
@@ -55,16 +57,16 @@ export class PizzaOrderComponent implements OnInit {
 
   pizzaSizes: any = {
     small: {
-      rate: 5
+      rate: 5.00
     },
     medium: {
-      rate: 7
+      rate: 7.00
     },
     large: {
-      rate: 8
+      rate: 8.00
     },
     extralarge: {
-      rate: 9
+      rate: 9.00
     }
   }
 
@@ -78,7 +80,11 @@ export class PizzaOrderComponent implements OnInit {
   ngOnInit() {
   }
 
-  chooseItem(e, obj) {
+  ngAfterViewInit() {
+    // this.inputButtons();
+  }
+
+  chooseToppingItem(e, obj) {
     let target = e.currentTarget,
       id = target.getAttribute('id'),
       value = target.value,
@@ -94,82 +100,197 @@ export class PizzaOrderComponent implements OnInit {
     }
 
     if (item.checked) {
-      this.order[size].push(item)
+      this.order[size].forEach((val, i) => {
+        this.order[size][i].toppings.push(item)
+      })
     } else {
       let index;
-      let checkItem = this.order[size].find((element, i) => {
+      // this.order[size].forEach((val, i) => {
+      let checkItem = this.order[size][0]["toppings"].find((element, j) => {
         if (element.id === item.id) {
-          index = i;
+          index = j;
           return element;
         }
       });
-      this.order[size].splice(index, 1)
+      // })
+      this.order[size].forEach((val, k) => {
+        this.order[size][k].toppings.splice(index, 1)
+      })
     }
-    // console.log(this.order.large)
+    console.log(this.order.medium)
     this.checkPromotions();
+  }
+
+  plusminus(e) {
+    let target = e.currentTarget;
+    let parent = $(target).parents('.quantity-btns');
+    let size = target.getAttribute('data-field');
+    let type = target.getAttribute('data-type');
+    var input = parent.find("input[name='" + size + "']");
+    var currentVal = parseInt(input.val());
+    if (!isNaN(currentVal)) {
+      if (type == 'minus') {
+
+        if (currentVal > input.attr('min')) {
+          currentVal--;
+          input.val(currentVal).change();
+          this.orderQuantity(size, currentVal, type)
+        }
+        if (parseInt(input.val()) == input.attr('min')) {
+          $(this).attr('disabled', true);
+        }
+
+      } else if (type == 'plus') {
+
+        if (currentVal < input.attr('max')) {
+          currentVal++;
+          input.val(currentVal).change();
+          this.orderQuantity(size, currentVal, type)
+        }
+        if (parseInt(input.val()) == input.attr('max')) {
+          $(this).attr('disabled', true);
+        }
+
+      }
+      this.checkPromotions();
+    } else {
+      input.val(0);
+    }
+
+  }
+
+  orderQuantity(size, currentVal, type) {
+    // if (currentVal > 0) {
+    if (type == 'plus') {
+      var cloneDeal = JSON.parse(JSON.stringify(this.order[size]));
+      // cloneDeal = cloneDeal.length>0 ? cloneDeal[0] : [];
+      if (cloneDeal.length > 0) {
+        this.order[size].push(cloneDeal[0]);
+      } else { // NO TOPPING ADDED BY DEFAULT
+        this.order[size].push({
+          rate: this.pizzaSizes[size].rate,
+          toppings: []
+        });
+      }
+    } else {
+      this.order[size].splice(0, 1);
+      if (this.order[size].length == 0) {
+        // let els = <HTMLCollection>document.getElementsByClassName(size + '-checkbox');
+        let els = document.getElementsByClassName(size + '-checkbox');
+        // let els = $('.'+size+'-checkbox');
+        console.log('els', els)
+        Array.prototype.forEach.call(els, function (el, i) {
+          el.checked = false;
+          console.log(el.checked);
+        });
+      }
+    }
+    // }
+    console.log(this.order)
   }
 
   checkPromotions() {
     // OFFER 1
-    if (this.order.medium.length >= 2) {this.offer1 = "Offer 1";} else { this.offer1 = "";}
+    if (this.order.small.length >= 1) {
+      this.order.small.forEach(el => {
+        if ('toppings' in el) {
+          let toppings = el.toppings;
+          if (toppings.length == 2) {
+            this.offer1 = "Offer 1 - Applied";
+          } else { this.offer1 = null; }
+        }
+      })
+    } else { this.offer1 = null; }
+
+    // OFFER 2
+    if (this.order.medium.length == 2) {
+      this.order.medium.forEach(deal => {
+        if ('toppings' in deal) {
+          let toppings = deal.toppings;
+          if (toppings.length == 4) {
+            this.offer2 = "Offer 2 - Applied";
+          } else { this.offer2 = null; }
+        }
+      })
+    } else { this.offer2 = null; }
 
     // OFFER 3
-    if (this.order.large.length >= 4) {
+    if (this.order.large.length >= 1) {
       let pep = false, bbq = false, matched = [];
-
-      this.order.large.forEach(el => {
-        if (el.name.toLowerCase() === 'pepperoni') pep = true;
-        if (el.name.toLowerCase() === 'barbecue chicken') bbq = true;
+      this.order.large.forEach(deal => {
+        if ('toppings' in deal) {
+          let toppings = deal.toppings;
+          toppings.forEach(topping => {
+            if (topping.name.toLowerCase() === 'pepperoni') pep = true;
+            if (topping.name.toLowerCase() === 'barbecue chicken') bbq = true;
+          })
+        }
       })
-      if (pep && bbq) {this.offer3 = "Offer 3";}
-      else {this.offer3 = "";}
-    } else {this.offer3 = "";}
+      if (pep && bbq) { this.offer3 = "Offer 3 - Applied"; }
+      else { this.offer3 = null; }
+
+    } else { this.offer3 = null; }
 
   }
 
-  getPrice(array, size) {
-    let sumPrice = 0;
-    if (array.length > 0) {
+  getPrice(orderArr, size) {
+    let sumPrice = 0, qty = 0, rate;
+    if (orderArr.length > 0) {
       switch (size) {
         case 'small':
-          sumPrice += 5.00;
+          sumPrice += this.pizzaSizes.small.rate;
           break;
         case 'medium':
-          sumPrice += 7.00;
+          sumPrice += this.pizzaSizes.medium.rate;
           break;
         case 'large':
-          sumPrice += 8.00;;
+          sumPrice += this.pizzaSizes.large.rate;
           break;
         case 'extralarge':
-          sumPrice += 9.00;;
+          sumPrice += this.pizzaSizes.extralarge.rate;
           break;
         default:
-          sumPrice += 0.00;;
+          sumPrice += 0.00;
       }
-
+      orderArr.forEach((element, i) => {
+        qty++;
+        if ('rate' in element) {
+          rate = parseFloat(element['rate']);
+        }
+        let toppings = 'toppings' in element ? element.toppings : [];
+        if (toppings.length > 0) {
+          toppings.forEach(topping => {
+            sumPrice += parseFloat(topping.rate);
+          })
+        }
+      });
+      sumPrice += (rate * (qty - 1)); // DEDUCT DEFAULT SIZE RATE FROM NO.OF QUANTITY AS IT HAS ALREADY BEEN ADDED ONCE IN SWITCH CASE ABOVE
     }
-
-    array.forEach(element => {
-      sumPrice += parseFloat(element.rate);
-    });
 
     // HANDLE OFFER 3
-    if ((array.length > 0 && size == 'large') && this.offer3 && sumPrice > 0) {
-      this.lgdiscount = (sumPrice / 2).toFixed(2);
+    if ((orderArr.length > 0 && size == 'large') && this.offer3 && sumPrice > 0) {
+      this.lgdiscount = (sumPrice / 2);
       console.log(this.lgdiscount)
       sumPrice = sumPrice;
-      return `<b>DP: $${this.lgdiscount}</b>`;
+      return `<b>DP: $${this.lgdiscount.toFixed(2)}</b>`;
       // return `<span class='strike'> $${sumPrice} </span> $${this.lgdiscount}`;
     }
-    // HANDLE OFFER 1
-    else if (this.offer1 && size == 'medium' && sumPrice > 0) {
-      this.mddiscount = (sumPrice - 5).toFixed(2);
-      this.showStriked = true;
-      return `<b>DP: $${this.mddiscount} </b>`;
+    // HANDLE OFFER 2
+    else if (this.offer2 && size == 'medium' && sumPrice > 0) {
+      this.mddiscount = this.pizzaSizes.medium.rate;
+      this.showDiscounted = true;
+      return `<b>DP: $9.00 </b>`;
       // return `<span class='strike'> $${sumPrice} </span> $${this.mddiscount}`;
     }
+    // HANDLE OFFER 1
+    else if (this.offer1 && size == 'small' && sumPrice > 0) {
+      this.smdiscount = this.pizzaSizes.small.rate;
+      this.showDiscounted = true;
+      return `<b>DP: $${this.smdiscount.toFixed(2)} </b>`;
+      // return `<span class='strike'> $${sumPrice} </span> $${this.smdiscount}`;
+    }
     else {
-      return `<b> ${sumPrice == 0 ? "" : '$'+sumPrice} </b>`;
+      return `<b> ${sumPrice == 0 ? "" : '$' + sumPrice.toFixed(2)} </b>`;
     }
   }
 }
